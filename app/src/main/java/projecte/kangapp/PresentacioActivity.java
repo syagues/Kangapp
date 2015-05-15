616f18dc56b158a8bdb7ca87f5772a0b9d1cddb6
syagues <sergiyr90@gmail.com>
2015-05-15 22:16:58 +0200

    Initial commit

---
+++ app/src/main/java/projecte/kangapp/PresentacioActivity.java
@@ -0,0 +1,43 @@
+package projecte.kangapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import projecte.llibreria.Techniques;
+import projecte.llibreria.YoYo;
+
+/**
+ * Created by sergi on 15/5/15.
+ */
+public class PresentacioActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presentacio);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                YoYo.with(Techniques.FadeOutUp)
+                        .duration(700)
+                        .playOn(findViewById(R.id.imageView));
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        iniciarApp();
+                    }
+                }, 1000);
+            }
+        }, 2000);
+    }
+
+    public void iniciarApp() {
+        Intent intent = new Intent(this, PrincipalActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/projecte/kangapp/PrincipalActivity.java
@@ -0,0 +1,102 @@
+package projecte.kangapp;
+
+import android.content.res.Configuration;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by sergi on 15/5/15.
+ */
+public class PrincipalActivity extends ActionBarActivity {
+
+    // DrawerLayout (menu lateral)
+    private ListView mDrawerList;
+    private DrawerLayout mDrawerLayout;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mActivityTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+        // Menu lateral (DrawerLayout)
+        mDrawerList = (ListView) findViewById(R.id.list_view);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+        addDrawerItems();
+        setupDrawer();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.primary)));
+    }
+
+    public void addDrawerItems(){
+        // Opcions menu lateral
+        mDrawerList = (ListView) findViewById(R.id.list_view);
+        final String[] opcions = {getResources().getString(R.string.str_buscar), getResources().getString(R.string.str_mis_articulos)};
+        mDrawerList.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, android.R.id.text1, opcions));
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Toast.makeText(EditActivity.this, "Opcio: " + opcions[i], Toast.LENGTH_SHORT).show();
+                mDrawerLayout.closeDrawers();
+
+                switch (i){
+                    case 0:
+                        finish();
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+    }
+
+    public void setupDrawer(){
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.str_drawer_open, R.string.str_drawer_close){
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                //getSupportActionBar().setTitle(getResources().getString(R.string.str_menu));
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+}
