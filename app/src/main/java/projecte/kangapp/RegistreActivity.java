618cab89ee1b6acf438944d003a984b74cb8e74e
syagues <sergiyr90@gmail.com>
2015-05-21 19:30:36 +0200

    Merge Branca Raul

--- app/src/main/java/projecte/kangapp/RegistreActivity.java
+++ app/src/main/java/projecte/kangapp/RegistreActivity.java
@@ -2,110 +2,121 @@ package projecte.kangapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.method.PasswordTransformationMethod;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
-import android.R.*;
 
 import projecte.animations.Techniques;
 import projecte.animations.YoYo;
 
 /**
- * Created by RAÜL on 18/05/2015.
+ * Created by RAÃœL on 18/05/2015.
  */
 public class RegistreActivity extends Activity {
 
     boolean cEnt = false;
     boolean nEnt = false;
 
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_registre);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registre);
 
-            final Button bregistre = (Button) findViewById(R.id.bRegistre);
-            final Button bacces = (Button) findViewById(R.id.bAcces);
-            final EditText etNom = (EditText) findViewById(R.id.etNom);
-            final EditText etContrasenya = (EditText) findViewById(R.id.etContrasenya);
+        final Button bregistre = (Button) findViewById(R.id.bRegistre);
+        final Button bacces = (Button) findViewById(R.id.bAcces);
+        final EditText etNom = (EditText) findViewById(R.id.etNom);
+        final EditText etContrasenya = (EditText) findViewById(R.id.etContrasenya);
+        etContrasenya.setTypeface(Typeface.DEFAULT);
+        etContrasenya.setTransformationMethod(new PasswordTransformationMethod());
 
 
-            etNom.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        etNom.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                nEnt=true;
+                if (cEnt==true){
+                    bacces.setEnabled(true);
                 }
+            }
+        });
 
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
+        etContrasenya.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                cEnt=true;
+                if (nEnt==true){
+                    bacces.setEnabled(true);
                 }
+            }
+        });
 
-                @Override
-                public void afterTextChanged(Editable s) {
-                    nEnt=true;
-                    if (cEnt==true){
-                        bacces.setEnabled(true);
-                    }
-                }
-            });
+        bregistre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iDades = new Intent(getApplicationContext(), DadesActivity.class);
+                startActivity(iDades);
+                finish();
 
-            etContrasenya.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-                }
+            }
 
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
+        });
 
-                }
+        bacces.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-                @Override
-                public void afterTextChanged(Editable s) {
-                    cEnt=true;
-                    if (nEnt==true){
-                        bacces.setEnabled(true);
-                    }
-                }
-            });
-
-            bregistre.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent iDades = new Intent(getApplicationContext(), DadesActivity.class);
-                    startActivity(iDades);
+                if (etNom.getText().toString().equals("Raul")) {
+                    Intent iPrincipal = new Intent(getApplicationContext(), PrincipalActivity.class);
+                    startActivity(iPrincipal);
                     finish();
-
-
+                } else {
+                    cEnt=false;
+                    nEnt=false;
+                    bacces.setEnabled(true);
+                    YoYo.with(Techniques.Shake).duration(500).playOn(findViewById(R.id.etNom));
+                    YoYo.with(Techniques.Shake).duration(500).playOn(findViewById(R.id.etContrasenya));
+                    etContrasenya.getText().clear();
+                    etContrasenya.getText().clear();
+                    Toast toast = Toast.makeText(getApplicationContext(), "EL USUARIO Y LA CONTRASEÑA NO COINCIDEN", Toast.LENGTH_LONG);
+                    LinearLayout linearLayout = (LinearLayout) toast.getView();
+                    TextView messageTextView = (TextView) linearLayout.getChildAt(0);
+                    messageTextView.setTextSize(12);
+                    toast.setGravity(Gravity.CENTER,0,0);
+                    toast.show();
                 }
 
-            });
+            }
+        });
 
-            bacces.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    if (etNom.getText().toString().equals("Raul")) {
-                        Intent iPrincipal = new Intent(getApplicationContext(), PrincipalActivity.class);
-                        startActivity(iPrincipal);
-                        finish();
-                    } else {
-                        cEnt=false;
-                        nEnt=false;
-                        bacces.setEnabled(true);
-                        YoYo.with(Techniques.Shake).duration(500).playOn(findViewById(R.id.etNom));
-                        YoYo.with(Techniques.Shake).duration(500).playOn(findViewById(R.id.etContrasenya));
-                        etContrasenya.getText().clear();
-                        etContrasenya.getText().clear();
-                        Toast.makeText(getApplicationContext(), "EL USUARIO Y LA CONTRASEÑA NO COINCIDEN", Toast.LENGTH_LONG).show();
-                    }
-
-                }
-            });
-
-        }
+    }
 
 }
