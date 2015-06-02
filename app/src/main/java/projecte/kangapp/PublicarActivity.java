28029fc0c930544cb37abf551d756bb232614927
syagues <sergiyr90@gmail.com>
2015-06-02 14:43:51 +0200

    Publicar

--- app/src/main/java/projecte/kangapp/PublicarActivity.java
+++ app/src/main/java/projecte/kangapp/PublicarActivity.java
@@ -1,42 +1,16 @@
 package projecte.kangapp;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Spinner;
 
-import com.mikepenz.materialdrawer.Drawer;
-import com.mikepenz.materialdrawer.accountswitcher.AccountHeader;
-import com.mikepenz.materialdrawer.model.DividerDrawerItem;
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
-import com.mikepenz.materialdrawer.model.SectionDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IProfile;
-import com.mikepenz.materialdrawer.util.DrawerImageLoader;
-import com.squareup.picasso.Picasso;
-
-import scrolls.ObservableScrollView;
-import scrolls.ObservableScrollViewCallbacks;
-import scrolls.ScrollState;
-import scrolls.ScrollUtils;
-
 /**
- * Created by RA�L on 21/05/2015.
+ * Created by Raül.
  */
 public class PublicarActivity extends AppCompatActivity {
 
